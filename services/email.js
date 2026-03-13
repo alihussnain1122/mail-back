03efebf72f4b4c742745c08e1a39251a88bd32b3
Ali <alihussnain.contact@gmail.com>
2026-03-14 01:50:14 +0500

    testing oauth connection

--- services/email.js
+++ services/email.js
@@ -2,6 +2,20 @@ import nodemailer from 'nodemailer';
 
 // Create transporter from request credentials
 export const createTransporterFromCredentials = (credentials) => {
+  if (credentials.authType === 'google' && credentials.googleAccessToken) {
+    return nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: credentials.emailUser,
+        accessToken: credentials.googleAccessToken,
+      },
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 15000,
+    });
+  }
+
   return nodemailer.createTransport({
     host: credentials.smtpHost,
     port: Number(credentials.smtpPort) || 587,
